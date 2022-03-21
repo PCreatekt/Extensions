b8127f0e2314451a27d0cfb48073f3d37b7c6d17
PHILGAMER <73665072+PHILGAMER@users.noreply.github.com>
2022-03-21 11:58:15 +0100

    Update Player.kt

--- src/main/kotlin/org/example/exampleplugin/extensions/Player.kt
+++ src/main/kotlin/org/example/exampleplugin/extensions/Player.kt
@@ -28,5 +28,7 @@ fun Player.killPlayer() {
 fun Player.clearInventory() {
     player!!.inventory.clear()
 }
-
+fun Player.boost(x: Int, y: Int, z: Int) {
+    player!!.velocity = Vector(x.toDouble(), y.toDouble(), z.toDouble())
+}
 
