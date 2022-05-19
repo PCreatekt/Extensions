1494359df64d8f0d6f10a9210cc5aedc63554f70
DasPhiller <73665072+DasPhiller@users.noreply.github.com>
2022-05-19 16:26:40 +0200

    Update MessagePrefix.kt

--- src/main/kotlin/org/example/exampleplugin/message/MessagePrefix.kt
+++ src/main/kotlin/org/example/exampleplugin/message/MessagePrefix.kt
@@ -1,4 +1,4 @@
-package de.dasphiller.peepo.message
+package org.example.exampleplugin.message
 
 enum class MessagePrefix(val prefix: String) {
     DEFAULT("<color:#999999>[<color:#3d85c6>PeepoSMP<color:#999999>]<color:#3d85c6> ")
