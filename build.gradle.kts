4e2a1ef9720c153e42958f964aa02c8b0df5f6d0
DasPhiller <73665072+DasPhiller@users.noreply.github.com>
2022-06-11 08:25:54 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,13 +3,13 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 val githubRepo = "DasPhiller/Extensions"
 
 plugins {
-    kotlin("jvm") version "1.6.10"
-    id("io.papermc.paperweight.userdev") version "1.3.5"
+    kotlin("jvm") version "1.7.0"
+    id("io.papermc.paperweight.userdev") version "1.3.6"
     `maven-publish`
 }
 
 group = "de.dasphiller.extensions"
-version = "1.1"
+version = "4.0"
 description = "Extensions for Paper/KSpigot"
 
 repositories {
@@ -19,10 +19,10 @@ repositories {
 
 dependencies {
     // PaperMC Dependency
-    paperDevBundle("1.18.2-R0.1-SNAPSHOT")
+    paperDevBundle("1.19-R0.1-SNAPSHOT")
 
     // KSpigot dependency
-    implementation("net.axay:kspigot:1.18.2")
+    implementation("net.axay:kspigot:1.19.0")
 }
 
 tasks {
